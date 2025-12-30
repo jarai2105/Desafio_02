8436eccdae6228a6a1f98aeca9b5fb69b3aa1ac2
Jaraí Santos <jaraisantos88@gmail.com>
2025-12-30 20:16:52 -0300

    Add files via upload

---
+++ desafio02.py
@@ -0,0 +1,11 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> dia = input("Digite o dia do nascimento: ")
+Digite o dia do nascimento: 21
+>>> mes = input ("Digite o mes do nascimento: ")
+Digite o mes do nascimento: 2
+>>> ano = input ("Digite o ano do nascimento: ")
+Digite o ano do nascimento: 1981
+>>> 
+>>> print (f"Data de nascimento: {dia}/{mes}/{ano}")
+Data de nascimento: 21/2/1981
